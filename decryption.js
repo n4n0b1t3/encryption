d07d7cd6b2aa4acb6d577c40ab47dd26ba645cd2
peterstadie <peter.stadie@protonmail.com>
2019-02-02 23:11:46 +0100

    changed to array of objects, added gcd method

--- decryption.js
+++ decryption.js
@@ -12,8 +12,63 @@ String.prototype.cleanup = function() {
 }
 
 function callback(action){
-	var cipherText = "JBDYZCLNHJBDOR";
+	// var cipherText = "JBDYZCLNHJBDOR";
+	var cipherText = document.getElementById("message").value.cleanup();
+	
+	
+	return function(){
+		var sequenceArray = [];
+		var k = 3; //keylength
+		var count = 0;
+		for(var pos=0; pos<cipherText.length-k; pos++){
+			var sequence = cipherText.substring(pos, pos+k);
+			let obj = sequenceArray.find(obj => obj.sequence == sequence);
+			
+			if(obj){
+				obj.positions.push(pos);
+			}else{
+				sequenceArray.push({sequence:sequence, positions:[pos]});
+			}
+		}
+		//.filter(obj => obj.positions > 3)
+		var gt4coinc = sequenceArray.filter(obj => obj.positions.length > 3);
+		gt4coinc.forEach(printObj);
+		
+		//get all numbers
+		var num = [];
+		gt4coinc.forEach(obj => num.push(obj.positions));
+		//console.log("num : " + num);
+		num = num.flat();
+		var gcds = [];
+		for(i=0; i<num.length-1; i++){
+			gcds.push(gcd(num[i],num[i+1]));
+		}
+		console.log("gcds: " + gcds);
+		
+		
+	}
+}
 
+function printObj(item, index){
+	console.log(JSON.stringify(item));
+}
+
+//greatest common divisor
+function gcd(a,b){
+	//console.log("ab:" + a + "," + b);
+	if ( ! b) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+	
+
+function old_callback(action){
+	// var cipherText = "JBDYZCLNHJBDOR";
+	var cipherText = document.getElementById("message").value.cleanup();
+	
+	
 	return function(){
 		var ary = {};
 		var k = 3; //keylength
@@ -26,7 +81,7 @@ function callback(action){
 				ary[ss] += "," + pos;
 			}
 		}
-		console.log(ary);
+		console.log(ary.filter((s,p) => p.length > 12));
 	}
 }
 
